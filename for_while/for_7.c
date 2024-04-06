ac084c5a7c3eaa954d7c3a802494abb34d222d84
hongsola92 <thfdkwl@kakao.com>
2024-04-07 00:45:01 +0900

    24-4-7_백준

---
+++ for_while/for_7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{   
+    int a,b;
+    int T;     
+    
+    scanf("%d", &T);  
+    
+    for(int i = 0; i < T; i++)
+    {  
+        int x;
+        x++;
+        scanf("%d %d", &a, &b);
+        printf("Case #%d: %d\n", x, a+b);
+    }
+
+    return 0;
+}
