44163ba4340191d263f606cdba7ee095057cf4f2
jinsung <js980902@gmail.com>
2024-04-07 20:30:10 +0900

    폴더구조생성

---
+++ stage_1/11382.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    long long A, B, C;
+    scanf("%lld %lld %lld", &A, &B, &C);
+    long long sum = A + B + C;
+    printf("%lld\n", sum);
+    return 0;
+}
---
+++ stage_1/2588.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num1, num2;
+    scanf("%d %d", &num1, &num2);
+
+    printf("%d\n", num1 * (num2 -((num2/10)  *10))); // 385 - 380 =5
+    printf("%d\n", num1 * (num2 - (((num2/100)*100) + (num2 -((num2/10)  *10)))) /10);
+    printf("%d\n", num1 * (num2/100));
+    printf("%d\n", num1 * num2);
+
+    return 0;
+}
