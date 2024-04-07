f871d44f45242febebd3201058812d6fbf4e775e
jobokuen <typhoon1019@gmail.com>
2024-04-07 22:47:14 +0900

    01_inputOutput

---
+++ 01_Input_Output_arithmetic_operations/02_A+B/main.c
@@ -0,0 +1,17 @@
+// 문제
+// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+// 입력
+// 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+// 출력
+// 첫째 줄에 A+B를 출력한다.
+
+#include<stdio.h>
+int main(void){
+    int A;
+    int B;
+    scanf("%d%d",&A,&B);
+    printf("%d",A+B);
+    
+}
---
+++ 01_Input_Output_arithmetic_operations/12_cat/main.c
@@ -0,0 +1,18 @@
+// 문제
+// 아래 예제와 같이 고양이를 출력하시오.
+
+// 입력
+// 없음.
+
+// 출력
+// 고양이를 출력한다.
+
+#include<stdio.h>
+
+int main(void){
+    printf("\\    /\\\n");
+    printf(" )  ( \')\n");
+    printf("(  /  )\n");
+    printf(" \\(__)|\n");
+    return 0;
+}
