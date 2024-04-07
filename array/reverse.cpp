1d4988148ae2ab632729a8e004ac47391495383e
kimwlsgh33 <kimwlsgh33@gmail.com>
2024-04-07 21:03:49 +0900

    Add Reverse algorithm

--- array/reverse.cpp
+++ array/reverse.cpp
@@ -7,20 +7,30 @@ int main() {
 
   int arr[N];
 
-  for (n = 0; n < N; ++n) {
-    arr[n] = n + 1;
+  // initilize [1:N]
+  for (n = 1; n <= N; ++n) {
+    arr[n - 1] = n;
   }
 
+  int tmp;
   for (int n1 = 0; n1 < M; ++n1) {
-    cin >> i >> j;
-    for (int n2 = i; n2 <= j; ++n2) {
-      // TODO: reverse
-      // 1 2 3 4 5
-      // 5 4 3 2 1
-      int tmp = arr[n2];
-      arr[n2] = arr[j];
-      arr[j] = tmp;
+    cin >> i >> j; // input range
+
+    // swap(a1, an), swap(a2, an-1), ... stop when it's middle
+    while (i < j) {
+      tmp = arr[i - 1];
+      arr[i - 1] = arr[j - 1];
+      arr[j - 1] = tmp;
+      ++i;
+      --j;
     }
+    // a2, a3, a4, a5, a6 => i: 2, j: 6 => (j - i + 1) => 5 (6 - 2 + 1)
+    // for (int n2 = 0; n2 < (j - i + 1) / 2; ++n2) {
+    //   // 1 2 3 4 5 => i: 1, j: 5
+    //   int tmp = arr[i - 1];
+    //   arr[i - 1] = arr[j - n2];
+    //   arr[j - n2] = tmp;
+    // }
   }
 
   for (n = 0; n < N; ++n) {
