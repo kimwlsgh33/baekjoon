ad62b19aa46588743d745f5863a777672e633a74
kimwlsgh33 <kimwlsgh33@gmail.com>
2024-04-11 00:52:48 +0900

    Solve Dial problem

--- string/5622_dial.c
+++ string/5622_dial.c
@@ -1,34 +1,69 @@
 #include <stdio.h>
 
 int main(void) {
-  // ['A', 'Z'] => [3, 4, ..., 10] => 10 - 3 + 1 => 8
-  int time[8];
-
-  // store elapse time * 2
-  for (int i = 3; i < 11; ++i) {
-    time[i - 3] = i * 2;
-  }
-
   // word : ['A', 'Z']
-  // 1. ABC : [65,67]
-  // 2. DEF : [68,70]
-  // 3. GHI : [6,73]
-  // ...
+  // len(word) : [2, 15]
+  // == 3 * 4 ==
+  // 3s.  ABC   : [65,67]
+  // 4s.  DEF   : [68,70]
+  // 5s.  GHI   : [71,73]
+  // 6s.  JKL   : [74,76]
+  // == 3 4 3 4 ==
+  // 7s.  MNO   : [77,79]
+  // 8s.  PQRS  : [80,83] 4
+  // 9s.  TUV   : [84,87]
+  // 10s. WXYZ  : [88,91] 4
   char word[16];
   scanf("%s", word);
 
   int idx = 0, total = 0;
-  // [first, last]
+  // while (word[idx] != '\0') {
+  //   //== 3 * 4 ==
+  //   if (word[idx] >= 'A' && word[idx] <= 'C')
+  //     total += 3;
+  //   else if (word[idx] >= 'D' && word[idx] <= 'F')
+  //     total += 3 + 1;
+  //   else if (word[idx] >= 'G' && word[idx] <= 'I')
+  //     total += 3 + 1 + 1;
+  //   else if (word[idx] >= 'J' && word[idx] <= 'L')
+  //     total += 3 + 1 + 1 + 1;
+  //   //== 3 4 3 4 ==
+  //   else if (word[idx] >= 'M' && word[idx] <= 'O')
+  //     total += 3 + 1 + 1 + 1 + 1;
+  //   else if (word[idx] >= 'P' && word[idx] <= 'S')
+  //     total += 3 + 1 + 1 + 1 + 1 + 1;
+  //   else if (word[idx] >= 'T' && word[idx] <= 'V')
+  //     total += 3 + 1 + 1 + 1 + 1 + 1 + 1;
+  //   else
+  //     total += 3 + 1 + 1 + 1 + 1 + 1 + 1 + 1;
+  // }
+
+  int AtoO;
   while (word[idx] != '\0') {
-    // elapse time [0, 7]
-    for (int i = 0; i < 8; ++i) {
-      // TODO: generalize sequence
-      if ('A' + i * 3 <= word[idx] && word[idx] <= 'C' + i * 3)
-        total += time[idx];
+    AtoO = 0;
+    // check 'A' ~ 'O'
+    for (int i = 0; i < 5; ++i) {
+      if (word[idx] >= 'A' + 3 * i && word[idx] <= 'C' + 3 * i) {
+        total += 3 + i;
+        AtoO = 1;
+      }
+    }
+
+    if (!AtoO) {
+      // check 'P' ~ 'S'
+      if (word[idx] >= 'P' && word[idx] <= 'S')
+        total += 8;
+      else if (word[idx] >= 'T' && word[idx] <= 'V')
+        total += 9;
+      else
+        total += 10;
     }
 
     idx++;
   }
+  // UNUCIC -> 868242 -> 979353 -> 36
+
+  printf("%d\n", total);
 
   return 0;
 }
