e3696dab3b4b6521e079ff60cbcba0d4ea44383c
kimwlsgh33 <kimwlsgh33@gmail.com>
2024-04-13 21:32:25 +0900

    Add dial example that more readable

--- string/5622_dial_2.c
+++ string/5622_dial_2.c
@@ -4,25 +4,23 @@ int main(void) {
   char word[16];
   scanf("%s", word);
 
-  int total = 0, idx = 0, n;
+  int total = 0, idx = 0;
   while (word[idx] != '\0') {
-    n = word[idx] - 'A';
-
-    if (n < 3) {
+    if (word[idx] < 'D') {
       total += 3;
-    } else if (n < 6) {
+    } else if (word[idx] < 'G') {
       total += 4;
-    } else if (n < 9) {
+    } else if (word[idx] < 'J') {
       total += 5;
-    } else if (n < 12) {
+    } else if (word[idx] < 'M') {
       total += 6;
-    } else if (n < 15) {
+    } else if (word[idx] < 'P') {
       total += 7;
-    } else if (n < 19) {
+    } else if (word[idx] < 'T') {
       total += 8;
-    } else if (n < 22) {
+    } else if (word[idx] < 'W') {
       total += 9;
-    } else if (n < 26) {
+    } else if (word[idx] <= 'Z') {
       total += 10;
     }
 
